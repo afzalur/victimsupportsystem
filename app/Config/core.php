35d48950fefee360d81c45b69cd423afef20ad1d
unknown <rajib1111@gmail.com>
2013-10-11 12:52:42 +0600

    config file updated

--- app/Config/core.php
+++ app/Config/core.php
@@ -33,7 +33,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+	Configure::write('debug',2);
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
@@ -239,7 +239,7 @@
  * Uncomment this line and correct your server timezone to fix
  * any date & time related errors.
  */
-	//date_default_timezone_set('UTC');
+	date_default_timezone_set('UTC');
 
 /**
  *
